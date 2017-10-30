f612bcc2b43c6beaca91dad721262fd88d27ab16
MelodyBibi <melody.scholtes@gmail.com>
2017-10-30 15:33:29 +0100

    Refactor : add a product class for manage comportment

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,92 +1,68 @@
 package com.gildedrose;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 class GildedRose {
-    Item[] items;
+
+    private final Collection<Product> products;
 
     public GildedRose(Item[] items) {
-        this.items = items;
+        products = new ArrayList();
+        for (Item item : items)
+            products.add(new Product(item));
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
 
-            Item product = items[i];
+        for (Product product : products) {
 
-            product = decrementDay(product);
+            product.decrementDay();
 
-            if(isNormalProduct(product)){
-                if(product.quality > 0)
-                    product.quality--;
-                if (isExpired(product) && product.quality > 0)
-                    product.quality--;
+            if(product.isNormalProduct()){
+                if(product.hasQualityStrictlyPositive())
+                    product.decrementQuality();
+                if (product.isExpired() && product.hasQualityStrictlyPositive())
+                    product.decrementQuality();
             }
 
-            if (!isNormalProduct(product) && product.quality > 0) {
-                if (product.quality < 50) {
-                    product.quality = product.quality + 1;
+            if (!product.isNormalProduct() && product.hasQualityStrictlyPositive()) {
+                if (product.hasQualityStrictlyBelow50()) {
+                    product.incrementQuality();
 
-                    if (isBackstagePasses(product)) {
-                        if (product.sellIn < 11) {
-                            if (product.quality < 50) {
-                                product.quality = product.quality + 1;
+                    if (product.isBackstagePasses()) {
+                        if (product.hasSellInBelow(11)) {
+                            if (product.hasQualityStrictlyBelow50()) {
+                                product.incrementQuality();
                             }
                         }
 
-                        if (product.sellIn < 6) {
-                            if (product.quality < 50) {
-                                product.quality = product.quality + 1;
+                        if (product.hasSellInBelow(6)) {
+                            if (product.hasQualityStrictlyBelow50()) {
+                                product.incrementQuality();
                             }
                         }
                     }
                 }
             }
 
-            if (!isNormalProduct(product) && product.sellIn < 0) {
-                if (!isAgedBrie(product)) {
-                    if (!isBackstagePasses(product)) {
-                        if (product.quality > 0) {
-                            if (!isSulfuras(product)) {
-                                product.quality = product.quality - 1;
+            if (!product.isNormalProduct() && product.hasSellInBelow(0)) {
+                if (!product.isAgedBrie()) {
+                    if (!product.isBackstagePasses()) {
+                        if (product.hasQualityStrictlyPositive()) {
+                            if (!product.isSulfuras()) {
+                                product.decrementQuality();
                             }
                         }
                     } else {
-                        product.quality = product.quality - product.quality;
+                        product.makeQualityAtZero();
                     }
                 } else {
-                    if (product.quality < 50) {
-                        product.quality = product.quality + 1;
+                    if (product.hasQualityStrictlyBelow50()) {
+                        product.incrementQuality();
                     }
                 }
             }
         }
-    }
-
-    private Item decrementDay(Item product) {
-        if (!isSulfuras(product)) {
-            product.sellIn = product.sellIn - 1;
-        }
-        return product;
-    }
-
-    private boolean isExpired(Item product) {
-        return product.sellIn <= 0;
-    }
-
-    private boolean isNormalProduct(Item product) {
-        return !isAgedBrie(product)
-                && !isBackstagePasses(product)
-                && !isSulfuras(product);
-    }
-
-    private boolean isAgedBrie(Item product) {
-        return product.name.equals("Aged Brie");
-    }
-
-    private boolean isBackstagePasses(Item product) {
-        return product.name.equals("Backstage passes to a TAFKAL80ETC concert");
-    }
-
-    private boolean isSulfuras(Item product) {
-        return product.name.equals("Sulfuras, Hand of Ragnaros");
     }
 }
---
+++ src/main/java/com/gildedrose/Product.java
@@ -0,0 +1,63 @@
+package com.gildedrose;
+
+public class Product {
+
+    private Item item;
+
+    public Product(Item item) {
+
+        this.item = item;
+    }
+
+    public void decrementDay() {
+        if (!isSulfuras()) {
+            item.sellIn--;
+        }
+    }
+
+    boolean isExpired() {
+        return item.sellIn <= 0;
+    }
+
+    boolean isNormalProduct() {
+        return !isAgedBrie()
+                && !isBackstagePasses()
+                && !isSulfuras();
+    }
+
+    boolean isAgedBrie() {
+        return item.name.equals("Aged Brie");
+    }
+
+    boolean isBackstagePasses() {
+        return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
+    }
+
+    boolean isSulfuras() {
+        return item.name.equals("Sulfuras, Hand of Ragnaros");
+    }
+
+    public boolean hasQualityStrictlyPositive() {
+        return item.quality > 0;
+    }
+
+    public void decrementQuality() {
+        item.quality--;
+    }
+
+    public void incrementQuality() {
+        item.quality++;
+    }
+
+    public boolean hasQualityStrictlyBelow50() {
+        return item.quality < 50;
+    }
+
+    public boolean hasSellInBelow(int sellIn) {
+        return item.sellIn < sellIn;
+    }
+
+    public void makeQualityAtZero() {
+        item.quality = 0;
+    }
+}
