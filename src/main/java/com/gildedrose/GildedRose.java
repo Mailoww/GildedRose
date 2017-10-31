a622f6106cec8dd7c25d81be59c12bffa496ed10
MelodyBibi <melody.scholtes@gmail.com>
2017-10-31 10:30:20 +0100

    Refactor : add an adapter to convert items to product

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -10,23 +10,10 @@ class GildedRose {
 
     public GildedRose(Item[] items) {
         this.items = items;
-        products = new ArrayList();
-        for (Item item : items){
-            Product product;
-            if(item.name.equals("Sulfuras, Hand of Ragnaros"))
-                product = Product.ofSulfuras(item);
-            else if(item.name.equals("Backstage passes to a TAFKAL80ETC concert"))
-                product = Product.ofBackstagePasses(item);
-            else if(item.name.equals("Aged Brie"))
-                product = Product.ofAgedBrie(item);
-            else
-                product = Product.ofNormal(item);
-            products.add(product);
-        }
+        products = ItemAdapter.convertToProduct(items);
     }
 
     public void updateQuality() {
-
         for (Product product : products) {
             product.decrementDay();
             product.updateQuality();
